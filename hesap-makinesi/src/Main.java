069e34000de4e29e6846aedc2005af30d6d73e36
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-04-17 23:30:18 +0300

    Hesap makinesi uygulaması ödevi hazırlandı

---
+++ hesap-makinesi/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        int islem, n1, n2;
+        Scanner input = new Scanner(System.in);
+        System.out.println("### HESAP MAKINESI ###");
+        System.out.println("Hesap makinesi uygulamasina hosgelniz. Lutfen yapmak istediginiz islemi seciniz.\n1. Toplama\t\t2. Cikarma\n3. Carpma\t\t4. Bolme");
+        System.out.print("Sectiginiz islemi yaziniz: ");
+        islem = input.nextInt();
+        switch (islem){
+            case 1:
+                System.out.println("Toplama islemini tercih ettiniz.");
+                System.out.print("Ilk sayiyi giriniz: ");
+                n1 = input.nextInt();
+                System.out.print("Ikinci sayiyi giriniz: ");
+                n2 = input.nextInt();
+                System.out.print("Toplama islemi sonucunuz: " + (n1 + n2));
+                break;
+            case 2:
+                System.out.println("Cikarma islemini tercih ettiniz.");
+                System.out.print("Ilk sayiyi giriniz: ");
+                n1 = input.nextInt();
+                System.out.print("Ikinci sayiyi giriniz: ");
+                n2 = input.nextInt();
+                System.out.print("Cikarma islemi sonucunuz: " + (n1 - n2));
+                break;
+            case 3:
+                System.out.println("Carpma islemini tercih ettiniz.");
+                System.out.print("Ilk sayiyi giriniz: ");
+                n1 = input.nextInt();
+                System.out.print("Ikinci sayiyi giriniz: ");
+                n2 = input.nextInt();
+                System.out.print("Carpma islemi sonucunuz: " + (n1 * n2));
+                break;
+            case 4:
+                System.out.println("Bolme islemini tercih ettiniz.");
+                System.out.print("Ilk sayiyi giriniz: ");
+                n1 = input.nextInt();
+                System.out.print("Ikinci sayiyi giriniz: ");
+                n2 = input.nextInt();
+                System.out.print("Bolme islemi sonucunuz: " + (n1 / n2));
+                break;
+            default:
+                System.out.print("Hatalı bir işlem seçtiniz!");
+                break;
+        }
+    }
+}
